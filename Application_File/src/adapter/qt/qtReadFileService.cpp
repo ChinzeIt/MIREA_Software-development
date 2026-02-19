3b523257004e7a9d312e06c0609c2ed76d021205
ChinzeIt <chinzeit@yandex.ru>
2026-02-19 20:31:13 +0300

    add fix for window without test

--- Application_File/src/adapter/qt/qtReadFileService.cpp
+++ Application_File/src/adapter/qt/qtReadFileService.cpp
@@ -4,12 +4,38 @@ std::string QTReadFileService::readFile (const std::string& rawPath) {
     QString QrawPath = QString::fromStdString(rawPath);
     std::string result = "Can not open file or content have damage";
 
+#if defined(Q_OS_WIN)
+    return fixWindowsRead(rawPath);
+#else
     QFile file(QrawPath);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) return result;
 
     result.clear();
     QTextStream in(&file);
     result = in.readAll().toStdString();
+
+    return result;
+#endif
+}
+// Not tested on OS "Windows"
+std::string QTReadFileService::fixWindowsRead(const std::string& rawPath) {
+    QString QrawPath = QString::fromStdString(rawPath);
+    std::string result = "Cannot open file or content is damaged";
+
+    QFileInfo fileInfo(QrawPath);
+    if (!fileInfo.exists() || !fileInfo.isFile()) return result;
+
+    QTemporaryFile tempFile(fileInfo.dir().absoluteFilePath("XXXXXX_temp.txt"));
+    if (!tempFile.open()) return result;
+
+    if (!QFile::copy(QrawPath, tempFile.fileName())) return result;
+
+    QTextStream in(&tempFile);
+    result.clear();
+    result = in.readAll().toStdString();
+
+    tempFile.close();
+    tempFile.remove();
 
     return result;
 }
--- Application_File/src/adapter/qt/qtReadFileService.h
+++ Application_File/src/adapter/qt/qtReadFileService.h
@@ -5,8 +5,12 @@
 #include <QString>
 #include <QFile>
 #include <QTextStream>
+#include <QFileInfo>
+#include <QDir>
+#include <QTemporaryFile>
 
 class QTReadFileService : public readFileService {
     public:
     std::string readFile(const std::string& rawPath) override;
+    std::string fixWindowsRead(const std::string& rawPath); // WARNING!!!
 };
